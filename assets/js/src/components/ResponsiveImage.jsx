c7c979f2cc4b2cf0e784c261304de59d6224f94e
luxumbra <hello@crafted.im>
2020-02-17 20:32:38 +0000

    test

--- assets/js/src/components/ResponsiveImage.jsx
+++ assets/js/src/components/ResponsiveImage.jsx
@@ -30,9 +30,7 @@ class ResponsiveImage extends React.Component {
    * @returns {undefined} undefined
    */
   componentDidMount() {
-    if (typeof window !== "undefined")
-      window.addEventListener("resize", this.onResize);
-
+    window.addEventListener("resize", this.onResize);
     this.onResize();
   }
 
@@ -55,8 +53,7 @@ class ResponsiveImage extends React.Component {
    * @returns {undefined} undefined
    */
   componentWillUnmount() {
-    if (typeof window !== "undefined")
-      window.removeEventListener("resize", this.onResize);
+    window.removeEventListener("resize", this.onResize);
   }
 
   /**
@@ -133,8 +130,8 @@ class ResponsiveImage extends React.Component {
     let currentImageSize;
     const { children } = this.props;
     const windowSize = {
-      width: typeof window !== "undefined" ? window.innerWidth : "",
-      height: typeof window !== "undefined" ? window.innerHeight : ""
+      width: window.innerWidth,
+      height: window.innerHeight
     };
 
     for (let i = 0; i < children.length; i++) {
